a528f206944ad60bfe294b6809acfff01e865571
nicolas roa <nrrodriguez2011@hotmail.com>
2018-07-03 00:54:17 -0500

    update data

--- app/views.py
+++ app/views.py
@@ -1,17 +1,16 @@
 from post import postfb, ite_groups
-from flask import request, redirect, render_template
+from flask import request, redirect, render_template, Flask
 from app import app
-from db import insert, formulario, table
-import time
+from db import insert, formulario, convert
 
 def users(tokens):
 	usuarios={}
 	usuario = 0
 	tokens = tokens.split(',')
 	for user in tokens:
-		if user!= '' :
+		if user!= "" :
 			usuario = usuario+1
-			usuarios['usuario'+ str(usuario)] ={'token': user ,'numeropost':0}
+			usuarios["usuario"+ str(usuario)] ={"token": user ,"numeropost":0}
 	return usuarios
 
 def IDgroups(grupos):
@@ -22,23 +21,7 @@ def IDgroups(grupos):
             groups.append(group)    
     return groups
 
-@app.route('/', methods=['GET'])
-def index():
-    result = table()
-    return render_template('index.html', result = result, formulario = formulario)
-
-@app.route('/', methods=['POST'])
-def params():   
-    message = request.form['post']
-    tokens = request.form['token']
-    grupos = request.form['grupos']
-    usuarios = users(tokens)
-    groups = IDgroups(grupos)
-    result = table()
-    if len(groups) >= 1 and len(usuarios) >= 1 and len(message) != 0:
-        ite_groups(groups,message,usuarios)
-        insert(groups, usuarios, message)
-        valor1 = 'parametros enviados'
-    else:
-        valor1 = 'faltan parametros'
-    return render_template('index.html', valor1 = valor1, result = result, formulario = formulario)
+@app.route('/publications')
+def publications():
+    valor = convert()
+    return render_template('datos.html',valor=valor)
--- db.py
+++ db.py
@@ -1,29 +1,40 @@
 from sqlalchemy import Table, Column, Integer, String, MetaData, create_engine, Sequence
-from sqlalchemy.sql import select
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import select
 import sqlalchemy
+import json
 
+data = []
 engine = create_engine('postgres://uwtmzhbfvujfkb:d66ebd309832961e7ba01b6eec6f964a28981c543f9f235f7650cb9108df07ac@ec2-54-235-70-127.compute-1.amazonaws.com:5432/dcvoji6oqnp1ab', echo=True)
-connection = engine.connect()
+con = engine.connect()
 metadata = MetaData()
 formulario = Table('formulario', metadata,
-    Column('id', Integer, primary_key=True),
-    Column('post', String),
-    Column('tokens', String),
-    Column('groups', String),
-    Column('notification', String)
+	Column('id', Integer, primary_key=True),
+	Column('post', String),
+	Column('tokens', String),
+	Column('groups', String),
+	Column('notification', String)
 )
 metadata.create_all(engine)
 
 def insert(groups, usuarios, message):
-	ins = formulario.insert().values(post=message, tokens=str(usuarios), groups=groups, notification='Done')
+	ins = formulario.insert().values(post=message, tokens=str(usuarios), groups=groups, notification="Done")
 	ins.compile().params
-	result = connection.execute(ins)
+	res = con.execute(ins)
 	ins.bind = engine
-	result.inserted_primary_key
+	res.inserted_primary_key
 
 def table():
 	s = select([formulario])
-	connection = engine .connect()
-	result = connection.execute("select * from formulario")
+	result = con.execute("select * from formulario").fetchall()
 	return result
+
+def convert():
+	for x in result:
+		file_json = { 'id' : x[0], 'post' : x[1], 'groups' : x[3], 'notification' : x[4]}
+		data.append(file_json)
+	json_ht = json.dumps(data)	
+	return json_ht
+
+result = table()
+so = convert()
